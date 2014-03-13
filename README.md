9984c425b4479ca49e38aca22c84172c7b7e4941
rohwinu <rohwinu@live.com>
2014-03-13 11:49:23 -0500

    c3

--- README.md
+++
@@ -1,4 +0,0 @@
-Plantilla
-=========
-
-Plantilla
--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-// jQUERY MOBILE CONFIGURATIONS
-// http://jquerymobile.com/test/docs/api/globalconfig.html
-// ---------------------------------------------------------------------------------
-//$(document).bind("mobileinit", function(){
-$(document).ready(function () {
-	MBP.startupImage(); // Show startup image. If you want to disable delete this line
-});
